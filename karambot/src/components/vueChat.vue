550df6d5668c6bbd599a539c600173c91e022192
Tintou <martin.lm@outlook.fr>
2019-12-03 13:33:32 +0100

    Début vueChat

---
+++ karambot/src/components/vueChat.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p> test </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'vueChat',
+  props: {
+    msg: String
+  }
+}
+</script>
--- karambot/src/views/Home.vue
+++ karambot/src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <vueChat msg="KaramBot"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import vueChat from '@/components/vueChat.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
-  }
+    vueChat
+  },
 }
 </script>
